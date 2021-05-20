6ae3192fc1e0a7ed52f1c5a8dd89f167fd9de6ed
Scoutshot <Scoutshot@DESKTOP-T2K1LL9>
2021-05-20 18:37:10 +0100

    Addition - Created Main Class File

---
+++ StockManagementSystem/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ * @author Scoutshot
+ * Title: Gwent Grails Stock Management System
+ *
+ */
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
