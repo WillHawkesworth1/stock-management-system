074b0402d4ffd9dae98bf3ef81931a423571012e
Scoutshot <Scoutshot@DESKTOP-T2K1LL9>
2021-05-20 18:44:25 +0100

    Addition - Created Test Window

--- StockManagementSystem/src/Main.java
+++ StockManagementSystem/src/Main.java
@@ -16,6 +16,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		JFrame loginPage = new JFrame("Login Page");
+		loginPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel pageTitle = new JLabel("Login Page", SwingConstants.CENTER);
+		pageTitle.setPreferredSize(new Dimension(300, 100));
+		loginPage.getContentPane().add(pageTitle, BorderLayout.CENTER);
+		
+		loginPage.setLocationRelativeTo(null);
+		loginPage.pack();
+		loginPage.setVisible(true);
 
 	}
 
