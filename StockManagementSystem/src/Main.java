770741cf710fea1d96b1b2ccd751a783564a954f
Scoutshot <Scoutshot@DESKTOP-T2K1LL9>
2021-05-21 15:27:12 +0100

    Addition - Added menu bar

--- StockManagementSystem/src/Main.java
+++ StockManagementSystem/src/Main.java
@@ -4,7 +4,7 @@
 
 /**
  * 
- * @author Scoutshot
+ * @author WIlliam Hawkesworth
  * Title: Gwent Grails Stock Management System
  *
  */
@@ -15,28 +15,45 @@ import javax.swing.*;
 public class Main {
 	
 	private static JFrame mainWindow;
-	private static JPanel loginPage;
+	private static JPanel loginPage, salesPage;
+	private static JMenuBar navigationBar;
+	private static JMenu navigationMenu, loginMenu;
+	private static JMenuItem overviewItem, stockItem, investmentsItem, costsItem, soldItem, weeklyReviewItem, monthlyReviewItem, usersItem, signOutItem;
+
 
 	public static void main(String[] args) {
 		
 		createWindow();
-
+		createMenuBar();
+		//createLoginPage();
+		createSalesPage();	
 	}
 	
-	//A method to create the window and display it to the user
+	//A method to create the window and display it to the user as well as creating instances of all the pages.
 	private static void createWindow() {		
 
+		//Creates instances of all the page panels.
 		loginPage = new JPanel();
+		salesPage = new JPanel();
 		
+		//Creates the window, setting the size and to close on exit.
 		mainWindow = new JFrame("Gwent Grails Stock Mananagement System");
 		mainWindow.setSize(800, 600);
 		mainWindow.setResizable(false);
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		//Adds the panels to the window.
 		mainWindow.add(loginPage);
+		mainWindow.add(salesPage);
+		
+		mainWindow.setVisible(true);
+	}
+	
+	private static void createLoginPage() {
 		
 		loginPage.setLayout((null));
 		
+		//Creates all the of the content, setting boundaries and information for each one.
 		JLabel titleLabel = new JLabel("Login Page");
 		titleLabel.setBounds(250, 20, 500, 80);
 		titleLabel.setFont (titleLabel.getFont ().deriveFont (48.0f));
@@ -69,7 +86,67 @@ public class Main {
 		gwentGrailsLogo.setBounds(0, 0, 100, 100);
 		loginPage.add(gwentGrailsLogo);
 		
-		mainWindow.setVisible(true);
+		
+		//Makes the login page visible or not on launch.
+		loginPage.setVisible(true);
+	}
+	
+	private static void createSalesPage() {
+		
+		salesPage.setLayout((null));
+		
+		//Creates all the of the content, setting boundaries and information for each one.
+		JLabel titleLabel = new JLabel("Stock Page");
+		titleLabel.setBounds(250, 20, 500, 80);
+		titleLabel.setFont (titleLabel.getFont ().deriveFont (48.0f));
+		salesPage.add(titleLabel);
+		
+		ImageIcon ggLogo = new ImageIcon("src/GwentGrailsLogo.jpg");
+		JLabel gwentGrailsLogo = new JLabel(ggLogo);
+		gwentGrailsLogo.setBounds(0, 0, 100, 100);
+		salesPage.add(gwentGrailsLogo);
+		
+		//Makes the login page visible or not on launch.
+		salesPage.setVisible(true);
+	}
+	
+	private static void createMenuBar() {
+navigationBar = new JMenuBar();
+		
+		navigationMenu = new JMenu("Navigation");
+		navigationBar.add(navigationMenu);
+		
+		loginMenu = new JMenu("Sign Out");
+		navigationBar.add(loginMenu);
+		
+		overviewItem = new JMenuItem("Overview");
+		navigationMenu.add(overviewItem);
+
+		stockItem = new JMenuItem("Stock");
+		navigationMenu.add(stockItem);
+		
+		investmentsItem = new JMenuItem("Investments");
+		navigationMenu.add(investmentsItem);
+		
+		costsItem = new JMenuItem("Costs");
+		navigationMenu.add(costsItem);
+		
+		soldItem = new JMenuItem("Sold");
+		navigationMenu.add(soldItem);
+		
+		weeklyReviewItem = new JMenuItem("Weekly Review");
+		navigationMenu.add(weeklyReviewItem);
+		
+		monthlyReviewItem = new JMenuItem("Monthly Review");
+		navigationMenu.add(monthlyReviewItem);
+		
+		usersItem = new JMenuItem("Users");
+		navigationMenu.add(usersItem);
+		
+		signOutItem = new JMenuItem("Sign Out");
+		loginMenu.add(signOutItem);
+		
+		mainWindow.setJMenuBar(navigationBar);
 	}
 	
 }
